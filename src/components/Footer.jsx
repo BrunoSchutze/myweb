6b12a8ce9c6d4016b7f5f4a54ffad0b14429b1e9
Bruno <brunoschutze8@gmail.com>
2025-12-12 20:15:26 -0300

    ultimodos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useContent } from "../ContentContext";
-import logoImg from "../assets/logo.png";
+import logoImg from "../public/logo.png";
 
 export default function Footer({ onLogoClick }) {
   const { content } = useContent();
